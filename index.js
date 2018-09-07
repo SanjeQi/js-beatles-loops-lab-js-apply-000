467bb4503abce632f20344ca0a3734fc345e8d43
SanjeQi <cristi.nord@gmail.com>
2018-09-07 22:29:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-// add solution here
-describe('Beatles Loops', function() {
-  it("returns an array of strings containing what instruments each musician plays", function() {
-    const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-    const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-
-    expect(theBeatlesPlay(musicians, instruments)).toEqual(["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums"]);
-  });
 
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
  function theBeatlesPlay(musicians, instruments) {
    let band = [];
-   for
+   for(i=0; i<musicians.length; i++){
+     let playsWhat = musicians[i] + ' plays ' + instruments[i];
+     band.push(playsWhat);
+   }
+   return band;
  }
